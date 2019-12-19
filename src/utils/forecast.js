8754b1074d91fd4a5274f3610eb795f1978bd3b0
Chris <c_schiebelbein@hotmail.com>
2019-12-19 13:57:13 -0700

    Update apparent temperature

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -5,14 +5,17 @@ const url = 'https://api.darksky.net/forecast/ea87953e23341f78b5993a2acbec31b2/'
 
 request({ url, json: true }, (error, response) => {
     //request({ url, json: true }, (error, {body})  --- This also works via destructuring
+    console.log(response.body)
     const {daily, currently, error: errorMsg} = response.body
     const weatherReport = 
         daily.data[0].summary +
           " It is currently " +
-          currently.temperature +
-          " degrees and there is a " +
+          currently.temperature + 
+          " degrees and feels like " +
+          currently.apparentTemperature +
+          " degrees celcius in current weather conditions. There is currently a " +
           currently.precipProbability +
-          "% chance of rain"
+          "% chance of rain."
     
     if (error) {
       callback("Unable to connect to weather service.", undefined);
